4b520f6ac9bb9d6e05f293c9e6cc2d17aeabd3be
Caroline Lopes <kaka201155@gmail.com>
2025-02-23 19:03:02 -0300

    feat: add cart item count display to Header component

--- src/components/Header/Header.styles.ts
+++ src/components/Header/Header.styles.ts
@@ -1,15 +1,54 @@
 import styled from 'styled-components';
 
-export const HeaderContainer = styled.header`
-  background: #0070f3;
-  padding: 20px;
-  color: white;
+export const HeaderWrapper = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  padding: 20px;
+  background-color: #333;
+  color: white;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 1000;
+
+  & > a {
+    text-decoration: none;
+  }
 `;
 
-export const Logo = styled.h1`
-  font-size: 24px;
-  margin: 0;
+export const CartIconContainer = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  cursor: not-allowed;
+`;
+
+export const CartCount = styled.span`
+  position: absolute;
+  top: -10px;
+  right: -10px;
+  background-color: red;
+  color: white;
+  border-radius: 50%;
+  padding: 5px 10px;
+  font-size: 12px;
+  font-weight: bold;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-width: 20px;
+  height: 20px;
+`;
+
+export const PageContent = styled.div`
+  margin-top: 80px;
+`;
+
+export const FakeStoreText = styled.span`
+  color: white;
+  font-size: 2rem;
+  font-weight: bold;
+  cursor: pointer;
 `;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,25 @@
-import { HeaderContainer, Logo } from './Header.styles';
+import React from 'react';
+import Link from 'next/link';
+import { useCartContext } from '@/contexts/CartContext';
+import { FaShoppingCart } from 'react-icons/fa';
+import { CartIconContainer, CartCount, HeaderWrapper, FakeStoreText } from './Header.styles';
 
-export default function Header() {
+const Header: React.FC = () => {
+  const { cartItems } = useCartContext();
+
+  const cartItemCount = cartItems.reduce((total, item) => total + item.quantity, 0);
+
   return (
-    <HeaderContainer>
-      <Logo>Fake Store</Logo>
-    </HeaderContainer>
+    <HeaderWrapper>
+      <Link href="/" passHref>
+        <FakeStoreText>Fake Store</FakeStoreText>
+      </Link>
+      <CartIconContainer>
+        <FaShoppingCart size={24} color="white" />
+        {cartItemCount > 0 && <CartCount>{cartItemCount}</CartCount>}
+      </CartIconContainer>
+    </HeaderWrapper>
   );
-}
+};
+
+export default Header;
