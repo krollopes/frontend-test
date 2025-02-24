45a97f8679bc6593b35c74206931755960f166a4
Caroline Lopes <kaka201155@gmail.com>
2025-02-23 23:27:37 -0300

    test: add unit test for Header component

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,10 +1,15 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import Header from './Header';
+import { CartProvider } from '@/contexts/CartContext';
 
 describe('Header', () => {
   it('should render the logo with correct text', () => {
-    render(<Header />);
+    render(
+      <CartProvider>
+        <Header />
+      </CartProvider>,
+    );
 
     // Verifica se o texto "Fake Store" está presente
     expect(screen.getByText('Fake Store')).toBeInTheDocument();
